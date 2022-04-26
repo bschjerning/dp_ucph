956e172823825c46b1c4b31deaa3b72fbf0b21ee
jem@rff.dk <jem@rff.dk>
2022-04-26 17:11:34 +0200

    Updated code

--- 3_discrete_continuous_choice/exercises/ex_ante/estimate_exante.py
+++ 3_discrete_continuous_choice/exercises/ex_ante/estimate_exante.py
@@ -4,40 +4,34 @@ import scipy.optimize as optimize
 import tools
 import model_exante as model
 
-def maximum_likelihood(model, est_par, theta0, data,do_stderr):
+def maximum_likelihood(par, est_par, theta0, data,do_stderr):
     
     # Check the parameters
     assert (len(est_par)==len(theta0)), 'Number of parameters and initial values do not match'
     
     #Estimation
-    # Fill in
+    obj_fun = lambda x: -log_likelihood(x,est_par,par,data)
+    res = optimize.minimize()
 
     return res
 
-def log_likelihood(theta, model, est_par, data):
+def log_likelihood(theta, est_par, par, data):
     
     #Update parameters
-    par = model.par
-    sol = model.sol
-
     par = updatepar(par,est_par,theta)
 
     # Solve the model
-    model.create_grids()
-    model.solve()
+    par = model.create_grids(par)
+    sol = model.solve(par)
 
     # Predict consumption
-    t = data.t
-    c_predict = tools.interp_linear_1d(sol.m[t,:],sol.c[t,:],data.m)
-    C_predict = c_predict*data.P        #Renormalize
 
     # Calculate errors
-    error = data.logC -np.log(C_predict)
+
 
     # Calculate log-likelihood
-    # Fill in
     
-    return np.mean(log_lik_vec) 
+    return f
 
 def updatepar(par, parnames, parvals):
 
@@ -47,53 +41,42 @@ def updatepar(par, parnames, parvals):
     return par
 
 def calc_moments(par,data):
-    agegrid = np.arange(par.moments_minage,par.moments_maxage+1)-par.age_min+1 # define the cell which correspond to the age we want the mean for. e.g. age 40-55 --> agegrid: 16-31
+    agegrid = np.arange(par.moments_minage,par.moments_maxage+1)-par.age_min+1
     return np.mean(data.A[agegrid,:],1)
 
-def method_simulated_moments(model,est_par,theta0,data):
 
+def method_simulated_moments(par,est_par,theta0,data):
 
     # Check the parameters
     assert (len(est_par)==len(theta0)), 'Number of parameters and initial values do not match'
     
     # Calculate data moments
-    data.moments = calc_moments(model.par,data)
+    data.moments = calc_moments(par,data)
 
     # Estimate
-    # Fill in
-    res = optimize.minimize(obj_fun,theta0, method='BFGS')
+    obj_fun = lambda x: sum_squared_diff_moments(x,est_par,par,data)
+    res = optimize.minimize()
 
     return res
 
-def sum_squared_diff_moments(theta,model,est_par,data):
 
-    par = model.par
+def sum_squared_diff_moments(theta,est_par,par,data):
+
     #Update parameters
     par = updatepar(par,est_par,theta)
 
     # Solve the model
-    model.create_grids()
-    model.solve()
+    par = model.create_grids(par)
+    sol = model.solve(par)
 
     # Simulate the momemnts
     moments = np.nan + np.zeros((data.moments.size,par.moments_numsim))
     for s in range(par.moments_numsim):
-
-        # Simulate
-        model.simulate()
-
-        #Calculate moments
-        moments[:,s] = calc_moments(par,model.sim)
+        print(s)
 
     # Mean of moments         
-    moments = np.mean(moments,1)
 
     # Objective function
-    if hasattr(par, 'weight_mat'):
-        weight_mat_inv = np.linalg.inv(par.weight_mat)  
-    else:
-        weight_mat_inv = np.eye(moments.size)   # The identity matrix and I^-1=I
     
-    diff = (moments-data.moments).reshape(moments.size,1)
 
-    return (# Fill in).ravel()
+    return obj
