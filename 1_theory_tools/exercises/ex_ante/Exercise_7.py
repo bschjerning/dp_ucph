a9192e0c3f8cd3ce21ceee8affe56376522a3558
Mada2150 <90259162+AdamHJorgensen@users.noreply.github.com>
2023-03-07 13:05:54 +0100

    Exercise 7-8 update
    Added hints and changed interpolation notes from 50 to 150 for better plot.

--- 1_theory_tools/exercises/ex_ante/Exercise_7.py
+++ 1_theory_tools/exercises/ex_ante/Exercise_7.py
@@ -50,10 +50,13 @@ def solve_deaton_infty(par):
     while (par.max_iter>= sol.it and par.tol<sol.delta):
         
         V0 = sol.V.copy()
-        interp = interpolate.interp1d(par.grid_W,V0, bounds_error=False, fill_value = "extrapolate") #Use this instead of np.interp, because it can extrapolate outside grid
-        
+        #This time we use scipy's interpolate function instead of numpy's
+        #Scipy's interpolater allows for extrapolation outside the grid
+        interp = interpolate.interp1d(par.grid_W,V0, bounds_error=False, fill_value = "extrapolate")
+
         for iw,w in enumerate(par.grid_W):
             #fill in
+            #Hint: Similar to Exercise_6
             
             
             
