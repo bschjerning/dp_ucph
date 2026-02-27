62edd9cfbfbc142101ae20280fe9f29f15d01688
qxb650 <qxb650@ku.alumni.ku>
2026-02-27 13:54:58 +0100

    Week 10 exercises and slides

--- 2_dynamic_discrete_choice/exercises/ex_ante/00_intro/simple_zurcher.py
+++ 2_dynamic_discrete_choice/exercises/ex_ante/00_intro/simple_zurcher.py
@@ -1,260 +1,228 @@
-
-# import packages used
+# import
 import numpy as np
 from scipy import interpolate
 
+def cost(x, par):
+    return par.c * x 
+
+def util(d, x, par):
+    return -cost(x,par) - par.RC*(d==1)
 
 def setup():   
     """
-    The function "setup" initializes and sets up the parameters and grids for a the simple Zurcher model.
+    Function initializes and sets up the parameters and grids for the simple zurcher model.
     
     Returns:
       the parameter object `par` which contains various parameters and grids used in the code.
     """
 
     class par: pass
-    
-    # Spaces
-    par.n = 175                      # Number of grid points
-    par.max = 450                    # Max of mileage
 
-    # structual parameters
-    par.RC = 11.7257                                     # Replacement cost
-    par.c = 2.45569 * 0.001                              # Cost parameter
-    par.beta = 0.99                                    # Discount factor
+    # 1. structual parameters
+    par.RC = 11.7257 # Replacement cost
+    par.c = 2.45569 * 0.001 # Cost parameter
+    par.beta = 0.99 # Discount factor
     
-    # Parameters for VFI
-    par.max_iter = 10000   # maximum number of iterations
+    # 2. parameters for VFI
+    par.max_iter = 10000 # maximum number of iterations
     par.tol = 1.0e-8 #convergence tol. level 
     
-    # Create grid
+    # 3. mileage grid
+    par.n = 175 # Number of grid points
+    par.max = 450 # Max of mileage
     par.grid = np.arange(0,par.n) # milage grid
     
-    # Create transition arrays - for exercise 2 and onwards
+    # 4. create transition arrays (exercise 2)
     par.transition = np.array([0,1,2]) # Transition grid
     par.p = np.array([1/3, 1/3, 1/3]) # Transition probability grid
     
-    # Simulated extreme value taste shocks -  for exercise 3 and onwards
+    # 5. draw extreme value type 1 / Gumbel taste shocks (exercise 3)
     np.random.seed(1987)
     par.num_eps = 10000
     par.sigma_eps = 1.0
     par.eps_keep_gumb = np.random.gumbel(loc=-par.sigma_eps * np.euler_gamma,scale=par.sigma_eps,size=par.num_eps)
     par.eps_replace_gumb = np.random.gumbel(loc=-par.sigma_eps * np.euler_gamma, scale=par.sigma_eps,size=par.num_eps)
     
-    # Gaussian taste shocks - for exercise 5
+    # 6. draw standard normal taste shocks (exercise 5)
     par.eps_keep_norm = np.random.normal(0,1,par.num_eps)
     par.eps_replace_norm = np.random.normal(0,1,par.num_eps)
     
-    # For vectorized
+    # 7. call function to make choice-dependent markov (transition) matrices
     par.P1 = create_transition_matrix(0, par)
     par.P2 = create_transition_matrix(1, par)
-    par.cost_grid = cost(par.grid, par)
         
     return par
 
 def solve_SA(par, vectorized = False, **kwargs):
     """
-    The function `solve_SA` solves a performs successive approximations(aka. value function iteration)    
-    Args:
-      par: The parameter `par` is an object that contains various parameters
-      vectorized: The "vectorized" parameter is a boolean flag that determines whether to use a
-    vectorized implementation of the Bellman equation solver or not. If set to True, the function
-    "bellman_vector" will be used to solve the Bellman equation. If set to False, the function "bellman.
-    Defaults to False
-    
-    Returns:
-      an object of the class `sol`, which contains the following attributes:
+    The function `solve_SA` performs successive approximations (VFI)
+    The function creates a class 'sol', containing:
     - `V`: a numpy array representing the value function
     - `pk`: a numpy array representing the policy function (conditional choice probabilities)
     - `it`: an integer representing the number of iterations performed
-    - `delta`: a float representing the difference between the current and previous value functions
+    - `delta`: a float representing the maximal difference between the current and previous value functions
     """
     
+    # 1. create class
     class sol: pass
-    sol.V = np.zeros([par.n]) #arbitrary starting values
-    sol.pk = np.zeros([par.n]) #arbitrary starting values
+
+    # 2. allocate
+    sol.V = np.zeros([par.n]) # arbitrary starting values
+    sol.pk = np.zeros([par.n]) # arbitrary starting values
+
+    # 3. initialize
     sol.it = 0
     sol.delta = 2000
         
-    while (par.max_iter>= sol.it and par.tol<sol.delta):
+    # 4. iterate
+    while (sol.it <= par.max_iter and sol.delta > par.tol):
         
+        # a. compute Bellman
         if vectorized==False:
             V_now, pk = bellman(sol.V, par, **kwargs)
+
         else:
             V_now, pk = bellman_vector(sol.V, par, **kwargs)
 
+        # b. update class
         sol.delta = np.amax(np.abs(V_now - sol.V))
         sol.it += 1
         sol.V = V_now
         sol.pk = pk
+
+    # 5. print
     print(f'Finished after {sol.it} iterations')
     print(f'Convergence achieved: {sol.delta < par.tol}')
     
     return sol
 
-
-
 def bellman(V_next, par, taste_shocks = 'None', stochastic_transition = False):
     """
-    The function `bellman` evaluates the integreated value bellmann-operator in a dynamic programming problem.
-    ie. For a given guess of the value function for the next period, it calculates a new guess of the value and also the choice probabilities.
-    problem using the Bellman equation, considering different types of taste shocks and transition
-    probabilities.
-    
-    Args:
-      V_next: The value function for the next period, which is a numpy array of size par.n.
-      par: The parameter object `par` contains various parameters.
-      taste_shocks: The parameter "taste_shocks" determines the type of taste shocks used in the
-    calculation.
-      stochastic_transition: The parameter "stochastic_transition" determines whether the transition
-    probabilities between states are stochastic or deterministic. If it is set to False, the transition
-    probabilities are deterministic. If it is set to True, the transition probabilities are stochastic.
-    Defaults to False
-    
-    Returns:
-      The function `bellman` returns two arrays: `V_now` and `pk`.
+    Function evaluates the integrated value bellmann-operator in a dynamic programming problem.
+    For a given guess of the (integrated) value function for the next period, it calculates a new guess
+    of the (integrated) value funtion and also the choice probabilities. The function is made general 
+    for with and without taste-shocks, stochastic state-transition, etc.
     """
     
-    V_now = np.zeros([par.n]) # Initialize value function array
-    pk = np.zeros([par.n]) # Initialize choice probability array
+    # 1. initialize
+    V_now = np.zeros([par.n])
+    pk = np.zeros([par.n])
     
-    for  x in par.grid: # Loop over states
+    # 2. loop over state grid
+    for x in par.grid:
             
-        # Calculate expected future value across states for each choice
-        if stochastic_transition == False: # Exercise 1
-            EV_keep = None # Fill in
-            EV_replace = None # Fill in
-        else: #Exercise 2
-            EV_keep = None # Fill in
-            EV_replace = None # Fill in
+        # a. compute expected future value for each choice, given the state: EV(x,d)
+        if stochastic_transition == False:
+            
+            # FILL IN. EXERCISE 1. Delete "None"
+            EV_keep = None 
+            EV_replace = None
 
-        # Calculate value of each choice
-        value_keep = None # Fill in
-        value_replace = None # Fill in
-        
-        # Find the maximum value across choices
-        maxV = None # Fill in
-        
-        ### Update value and choice
-        
-        # Exercise 1
-        if taste_shocks == 'None':
-            V_now[x] = None # Fill in
-            pk[x] = None # Fill in
-        
-        # Exercise 4
-        elif taste_shocks == 'Extreme Value':
-            V_now[x] = None # Fill in
-            pk[x] = None # Fill in
+
+
+
+        else:
             
+            # FILL IN. EXERCISE 2. Delete "None"
+            EV_keep = None
+            EV_replace = None
+
+
+
+
+        # b. Calculate value of each choice, "value-choice functions"
+        # FILL IN. EXERCISE 1. Delete "None"
+        value_keep = None
+        value_replace = None
+        maxV = None
+
+
+
         
-        # Exercise 3
-        elif taste_shocks == 'Monte Carlo Extreme Value':
-            values = np.column_stack([value_keep + par.eps_keep_gumb, value_replace + par.eps_replace_gumb])
+        # 3. Find the maximum value across choices -> the (integrated) value function
+        if taste_shocks == 'None':
+            # FILL IN. EXERCISE 1. Delete "None"
+            V_now[x] = None
+            pk[x] = None
+
+
+
+        
+        elif taste_shocks == 'Analytical: Extreme Value':
+            # FILL IN. EXERCISE 4. Delete "None"
+            V_now[x] = None
+            pk[x] = None
+
+
+
+            
+        elif taste_shocks == 'Monte Carlo: Extreme Value':
+            values = np.column_stack([value_keep + par.eps_keep_gumb, value_replace + par.eps_replace_gumb]) # shape (numeps, 2)
             choices = np.argmax(values, axis = 1)
 
-            V_now[x] = values[np.arange(par.num_eps), choices].mean()
+            V_now[x] = np.mean(np.max(values, axis=1)) # 1) take max of each row 2) take mean of all maxes'
             pk[x] = 1 - choices.mean()
         
-        # Exercise 5
-        elif taste_shocks == 'Normal':
-            V_now[x] = None # Fill in
-            pk[x] = None  # Fill in           
+        elif taste_shocks == 'Monte Carlo: Normal':
+            # FILL IN. EXERCISE 5. Delete "None". Hint: Just like when we drew the Gumbel taste shocks
+            V_now[x] = None
+            pk[x] = None         
+
+
+
+
     return V_now, pk
-
-
-def cost(x, par):
-    """
-    The cost function calculates the cost of bus maintenance.
-    
-    Args:
-      x: The parameter "x" represents the current state of the system.
-      par: The parameter object `par` contains various parameters.
-
-    Returns:
-      The cost of bus maintenance.
-    """
-    return par.c * x 
-
-def util(d, x, par):
-    """
-    The function calculates the utility based on the choice and state.
-    
-    Args:
-      d: The parameter "d" represents a decision variable.
-      x: The parameter `x` is a variable that represents a state. 
-      par: The parameter object `par` contains various parameters.
-    
-    Returns:
-      The utility of a choice given the state.
-    """
-    
-    if d == 0:
-        return - cost(x, par)
-    if d == 1:
-        return - par.RC - cost(0, par)
 
 def EV_deterministic(d, x, V_next, par):
     """
-    The function `EV_deterministic` calculates the expected value of the next period's value function
-    given the current state and decision.
-    
-    Args:
-      d: The parameter "d" represents the probability of an event occurring. It is a value between 0 and
-    1, where 0 means the event will not occur and 1 means the event will definitely occur.
-      x: The parameter "x" represents the current state of the system.
-      V_next: The parameter V_next represents the value function for the next period. It is a vector
-    that contains the value of each state in the next period.
-      par: The parameter object `par` contains various parameters.
-    
-    Returns:
-      the value of next period
+    A function that computes next-period expected value function conditional on choice d: EV(x,d),
+    where the expectation is wrt. the state variable, i.e. no stochasticity here
     """
-    x_next = None # fill in
-    x_next = np.fmin(x_next, par.n-1) # Ensure that x_next is within grid
-    return None # Fill in
+
+    # FILL IN. EXERCISE 1. Delete "None"
+    x_next = None
+    EV = None
+
+    
+
+    return EV
 
 def EV_stochastic(d, x, V_next, par):
     """
-    The function `EV_stochastic` calculates the expected value of a variable `V_next` given a set of
-    probabilities `par.p` and transition probabilities `par.transition`.
-    
-    Args:
-      d: The parameter "d" represents the probability of an event occurring. It is a value between 0 and
-    1, where 0 means the event will not occur and 1 means the event will definitely occur.
-      x: The parameter "x" represents the current state of the system.
-      V_next: The parameter V_next represents the value function for the next period. It is a vector
-    that contains the value of each state in the next period.
-      par: The parameter object `par` contains various parameters.
-    
-    Returns:
-      the expected value (EV) of the next period's value function (V_next) given the current state (x),
-    the decision variable (d), and the transition probabilities (par.transition) and probabilities
-    (par.p).
+    A function that computes next-period expected value function conditional on choice d: EV(x,d),
+    where the expectation is wrt. the state variable
     """
-    pass # Fill in
+
+    # FILL IN. Delete "None".
+    EV = None
+
+
+
     
-    return None # Fill in
+    return EV
 
 def bellman_vector(V_next, par):
     """
-    The function `bellman_vector` calculates the value and choice probabilities for each choice in a
-    dynamic programming problem using the Bellman equation.
-    
-    Args:
-      V_next: V_next is a vector representing the value function in the next period. It has dimensions
-    (n, 1), where n is the number of states in the model. Each element of V_next represents the value of
-    being in a particular state in the next period.
-      par: The parameter `par` is an object that contains various parameters.
-    
-    Returns:
-      the updated value of the current state (V_now) and the probability of choosing to replace the
-    current state (pk).
+    Function calculates the value and choice probabilities for each choice in a
+    dynamic programming problem using the Bellman equation, vectorized
     """
     
-    pass # Fill in
+    # FILL IN. EXERCISE 6. Delete "None".
+    # Hint: The function should follow the structure of "bellman" but you have to code
+    # up some of the auxilary functions that we use, e.g. EV_stochastic
+    
+    # structure of function "bellman":
+    # 1) compute EV(x,d) using Markov (transition) matrices
+    # 2) compute value of each choice, "value-choice functions"
+    # 3) compute value function by features of Gumbel distribution
+
+    V_now = None
+    pk = None
+
+
+
             
-    return None # Fill in
+    return V_now, pk
 
 def create_transition_matrix(d, par):
     """
@@ -271,22 +239,22 @@ def create_transition_matrix(d, par):
     Returns:
       the transition matrix, which is a numpy array of shape (n, n).
     """
-    # p = np.append(par.p,1-np.sum(par.p)) # Get transition probabilities
-    P = np.zeros((par.n,par.n)) # Initialize transition matrix
-    
-    if d == 0: # keep
-        # Loop over rows
-        for i in range(par.n):
-            # Check if p vector fits entirely
-            if i <= par.n-len(par.p):
-                P[i][i:i+len(par.p)]=par.p
-            else:
-                P[i][i:] = par.p[:par.n-len(par.p)-i]
-                P[i][-1] = 1.0-P[i][:-1].sum()
 
-    if d == 1: # replace
-        # Loop over rows
-        for i in range(par.n):
-            P[i][:len(par.p)]=par.p
+    # 1. initialize Markov (transition) matrix
+    P = np.zeros((par.n,par.n)) 
+    
+    # 2. keep
+    if d == 0:
+        for i in range(par.n): # loop over rows
+            if i <= par.n-len(par.p): # check if p vector fits entirely
+                P[i,i:i+len(par.p)]=par.p # slice row i and columns i till i + 3 (length of vector of shocks m)
+            else: # truncate probabilities: cant go "outside" state grid
+                P[i,i:] = par.p[:par.n-len(par.p)-i]
+                P[i,-1] = 1.0-P[i][:-1].sum()
+
+    # 3. replace
+    if d == 1:
+        for i in range(par.n): # loop over rows
+            P[i][:len(par.p)]=par.p # fill in probabilities
     
     return P
